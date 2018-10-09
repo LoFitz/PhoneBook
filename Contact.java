58222f363cab9a4eb57ccc8f6a15eade8d330878
LoFitz <44005289+LoFitz@users.noreply.github.com>
2018-10-09 12:54:42 -0700

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,74 @@
+public class Contact {
+	
+	private String name;
+	private String phoneNum;
+	private String email;
+	private String notes;
+	
+	/**
+	 * creates a contact object with no parameters.  Can set the parameters later
+	 */
+	public Contact () {
+		this.name;
+		this.phoneNum;
+		this.email;
+		this.notes;
+	}
+	
+	/**
+	 * 
+	 * @param thisName - name used to change the name in the contact object
+	 */
+	public static void setName(String thisName) {
+		
+	}
+	/**
+	 * 
+	 * @return - name of the contact object
+	 */
+	public static String getName() {
+		
+	}
+	/**
+	 * 
+	 * @param thisNum - phone number of the contact object
+	 */
+	public static void setPhoneNum(String thisNum) {
+		
+	}
+	/**
+	 * 
+	 * @return - phone number of the contact object
+	 */
+	public static String getPhoneNum() {
+		
+	}
+	/**
+	 * 
+	 * @param thisEmail - email for contact object
+	 */
+	public static void setEmail(String thisEmail) {
+		
+	}
+	/**
+	 * 
+	 * @return - email in the contact
+	 */
+	public static String getEmail() {
+		
+	}
+	/**
+	 * 
+	 * @param thisNote - set the notes for the contact object
+	 */
+	public static void setNotes(String thisNote) {
+		
+	}
+	/**
+	 * 
+	 * @return - notes in the contact object
+	 */
+	public static String getNotes() {
+		
+	}
+}
