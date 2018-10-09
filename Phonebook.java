4cfe027a88b0760ae26f5658b0caae107686a29f
LoFitz <44005289+LoFitz@users.noreply.github.com>
2018-10-09 12:52:29 -0700

    Create Phonebook.java

---
+++ Phonebook.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Phonebook implements comparable{
+	
+	ArrayList <Contact> Contacts;
+	/**
+	 * format and print all of the contacts in the contacts array in the first interface of the phone book
+	 */
+	public String toString() {
+		
+	}
+	/**
+	 * remove a contact object from the contacts array list
+	 * @param thisOne - index of the contact we want to remove
+	 */
+	public static void deleteContact(int thisOne) {
+		
+	}
+	/**
+	 * creates a new contact and sets all variables and adds to the Contacts array list
+	 * @param Name - name of the contact we want to add
+	 * @param PhoneNum - number of the contact we want to add
+	 * @param Email - email of the contact we want to add
+	 * @param Notes - notes of the contact we want to add
+	 */
+	public static void addContact(String Name, String PhoneNum, String Email, String Notes) {
+		
+	}
+	/**
+	 * Change an attribute of the contact object
+	 * @param option - integer representing the option of which attribute of the contact they want to modify
+	 */
+	public static void modifyContact(int option) {
+		
+	}
+	/**
+	 * re-alphabetize the contacts array list by the names
+	 */
+	public static void sort() {
+		
+	}
+}
